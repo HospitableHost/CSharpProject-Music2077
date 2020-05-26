ba9114eefb625f40e13a6f9d01736181c719ab64
党自强 <44576391+HospitableHost@users.noreply.github.com>
2020-05-26 18:34:24 +0800

    Score类新增变量totalScore

---
+++ Assets/Score/Scripts/Score.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using UnityEngine;
+
+namespace Score
+{
+    public class Score
+    {
+        public int totalScore { get; set; }  //总分
+    }
+
+}
