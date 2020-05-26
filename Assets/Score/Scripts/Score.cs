c1635e11dbf8c2e6080085aa8abac0fcdb6624dc
Wiki <52619695+Wik-i@users.noreply.github.com>
2020-05-26 20:48:15 +0800

    Score类新增属性perfectNum

--- Assets/Score/Scripts/Score.cs
+++ Assets/Score/Scripts/Score.cs
@@ -7,11 +7,12 @@ namespace Score
     public class Score
     {
         public int totalScore { get; set; }  //总分
-<<<<<<< HEAD
+
         public int missNum { get; set; }   //miss掉的方块数
-=======
+
         public int goodNum { get; set; }  //good级命中数
->>>>>>> 9b03847c00b4d6f664db587784ae0c8c00e2b1c2
+
+        public int perfectNum { get; set; } //perfect评分方块数
     }
 
 }
