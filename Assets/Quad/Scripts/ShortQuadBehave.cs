3319b0826b1729fd75cc4b202bacc35abb1c7371
Wiki <52619695+Wik-i@users.noreply.github.com>
2020-05-28 17:57:37 +0800

    实现ShortQuadBehave中checkhit方法和markrecording方法

--- Assets/Quad/Scripts/ShortQuadBehave.cs
+++ Assets/Quad/Scripts/ShortQuadBehave.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Score;
 
 public class ShortQuadBehave : QuadBehave
 {
@@ -13,9 +14,41 @@ public class ShortQuadBehave : QuadBehave
     }
 
     //todo:检测触摸判断
-    override public bool CheckHit()
+    override public bool CheckHit(Score.Score scoreBoard)
     {
-        return false;
+        int score = 0;
+        Vector3 CameraPos = new Vector3(0, 0, -5);//摄像机的世界坐标
+        Vector3 TouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (TouchPos.z > 5)//在合法触摸区之外
+        {
+            return false;
+        }
+
+        RaycastHit result;
+        if (Physics.Raycast(CameraPos, TouchPos, out result))
+        {
+            double distance = this.m_nowPos.z;//获取按键的z坐标
+
+            if (distance > erreurMiss)
+            {
+                score = 0;
+            }
+            else if (distance > erreurGood)
+            {
+                score = 10;//good 得分
+            }
+            else if (distance > -erreurPerfect)
+            {
+                score = 20;//完美得分
+            }
+
+            this.MarkRecording(scoreBoard, score);
+            this.IsValid = false;//触摸一次之后按键失效处理
+            return true;//成功触摸
+        }
+
+
+        return false;//在触摸区内但是没有触摸
     }
     //todo:出边界检测
     public override bool CheckOut()
@@ -29,8 +62,22 @@ public class ShortQuadBehave : QuadBehave
             return false;
     }
     //todo:统计分数加到计分板上
-    override public void MarkRecording()
+    override public void MarkRecording(Score.Score scoreBoard, int ScoreToAdd)
     {
+
+        scoreBoard.totalScore += ScoreToAdd;
+        if (ScoreToAdd == 20)//perfect
+        {
+            scoreBoard.perfectNum++;
+        }
+        else if (ScoreToAdd == 10)//good
+        {
+            scoreBoard.goodNum++;
+        }
+        else//others are miss
+        {
+            scoreBoard.missNum++;
+        }
 
     }
 
