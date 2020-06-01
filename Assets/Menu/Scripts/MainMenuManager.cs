277f06b8f1b4dc34ecbf4c7c73f243acbbe6064d
Wiki <52619695+Wik-i@users.noreply.github.com>
2020-06-01 12:09:51 +0800

    更新了主界面和选歌界面的转场方法
    原来的转换画面方式是老方法，已经被弃用，现在更新为scenemanager方法

--- Assets/Menu/Scripts/MainMenuManager.cs
+++ Assets/Menu/Scripts/MainMenuManager.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
     public void StartGameOnClick(string sceneName)
     {
-        Application.LoadLevel(sceneName);
+        //Application.LoadLevel(sceneName);
+        SceneManager.LoadScene("sceneName");
     }
 }
--- Assets/Menu/Scripts/SelectSongManager.cs
+++ Assets/Menu/Scripts/SelectSongManager.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SelectSongManager : MonoBehaviour
 {
     public void SelectButtonOnClick(string sceneName)
     {
-        Application.LoadLevel(sceneName);
+        //Application.LoadLevel(sceneName);
+        SceneManager.LoadScene("sceneName");
     }
 
 }
