1626b6694ad9246e1edd8707f3ae3e8dd73458bd
ztrbq <45473111+ztrbq@users.noreply.github.com>
2020-06-19 23:20:57 +0800

    321

--- Assets/MusicPlayer/Scripts/CountDown.cs
+++ Assets/MusicPlayer/Scripts/CountDown.cs
@@ -8,15 +8,15 @@ public class CountDown : MonoBehaviour
     GameObject number2;
     GameObject number3;
     GameObject start;
-
+    AudioSource AS;
     private void Start()
     {
+        AS = gameObject.GetComponent<AudioSource>();
+        AS.clip.name = Settings.Settings.ChosenSong;
         Debug.Log("having readyToStart");
         GameObject q = Resources.Load<GameObject>("Prefabs/number1Text");
-        //Debug.Log(1);
         number1 = MonoBehaviour.Instantiate(q, new Vector3(0, 0, 50.0f), new Quaternion());
 
-        //Debug.Log(2);
         q = Resources.Load<GameObject>("Prefabs/number2Text");
         number2 = MonoBehaviour.Instantiate(q, new Vector3(0, 0, 50.0f), new Quaternion());
 
@@ -25,54 +25,38 @@ public class CountDown : MonoBehaviour
 
         q = Resources.Load<GameObject>("Prefabs/startText");
         start = MonoBehaviour.Instantiate(q, new Vector3(0, 0, 50.0f), new Quaternion());
+        AS.Stop();
         StartCoroutine(Count());
     }
-    private IEnumerator Count()
+    public IEnumerator Count()
     {
+        Debug.Log(number1);
+        number1.transform.position.Set(0, 0, 50);
+        number2.transform.position.Set(0, 0, 50);
+        number3.transform.position.Set(0, 0, 50);
+        start.transform.position.Set(0, 0, 50);
         Time.timeScale = 0;
         Debug.Log("Count start");
         for (float t = 0; t <= 55.0f; t += 1f)
         {
-            Debug.Log("携程");
             number3.transform.position -= new Vector3(0, 0, 1f);
             yield return 0;
         }
         for (float t = 0; t <= 55.0f; t += 1f)
         {
-            Debug.Log("携程");
             number2.transform.position -= new Vector3(0, 0, 1f);
             yield return 0;
         }
         for (float t = 0; t <= 55.0f; t += 1f)
         {
-            Debug.Log("携程");
             number1.transform.position -= new Vector3(0, 0, 1f); ;
             yield return 0;
         }
         for (float t = 0; t <= 55.0f; t += 1f)
         {
-            Debug.Log("携程");
             start.transform.position -= new Vector3(0, 0, 1f);
             yield return 0;
         }
-        //nowPos.Set(0.3f, 0.6f, 50f);
-        //for (float t = 0; t <= 10.0f; t += 1.0f)
-        //{
-        //    nowPos.Set(nowPos.x, nowPos.y, nowPos.z - 100 * Time.deltaTime);
-        //    number2.transform.position = nowPos;
-        //}
-        //nowPos.Set(0.3f, 0.6f, 50f);
-        //for (float t = 0; t <= 10.0f; t += 1.0f)
-        //{
-        //    nowPos.Set(nowPos.x, nowPos.y, nowPos.z - 100 * Time.deltaTime);
-        //    number1.transform.position = nowPos;
-        //}
-        //nowPos.Set(0.3f, 0.6f, 50f);
-        //for (float t = 0; t <= 10.0f; t += 1.0f)
-        //{
-        //    nowPos.Set(nowPos.x, nowPos.y, nowPos.z - 100 * Time.deltaTime);
-        //    start.transform.position = nowPos;
-        //}
-
+        AS.Play();
     }
 }
