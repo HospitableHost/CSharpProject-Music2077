bbd6ec8bcb73030dccd7eb5bfa4c5f22f7c2bb75
EdwardLi250 <3494855836@qq.com>
2020-06-20 02:16:45 +0800

    增加开始发射音符前的时间

--- Assets/Shooter/Scripts/ShooterBehave.cs
+++ Assets/Shooter/Scripts/ShooterBehave.cs
@@ -14,6 +14,7 @@ public class ShooterBehave : MonoBehaviour
     private int i;    //对List列表进行计数
     private bool[] Track;        //存储12条通道是否使用的数组
     private List<Note.Note> Music;   //深复制存储乐谱的链表
+    float startTime = 0.0f;
     private void Shoot()    //根据乐谱链表，发射一个Quad
     {
         switch (Music[i].noteType)  //按类型绑定脚本
@@ -57,13 +58,14 @@ public class ShooterBehave : MonoBehaviour
         }
         i = 0;
         time = 50 / QuadBehave.m_vel;
+        startTime = Time.time;
     }   
     void FixedUpdate()    //根据时间判断发射与否
     {       
         if (i < Music.Count)
         {       
             Note.Note currentNote = Music[i];
-            if (Time.time >= currentNote.arrivalTime-time)
+            if (Time.time >= currentNote.arrivalTime - time + startTime)
             {
                 if (i == 0) Background.Background.PlanesActivity = false;
                 Shoot();
